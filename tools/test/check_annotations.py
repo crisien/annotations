947636ce5ccce760b81858b73972d3001d198152
Friedrich Knuth <knuth@marine.rutgers.edu>
2017-03-24 11:23:03 -0400

    added tests for valid time stamps and intervals

---
+++ tools/test/check_annotations.py
@@ -0,0 +1,58 @@
+import os
+import pandas as pd
+
+
+
+def check_valid_time(data, root, filename):
+	# iterate over start and end times
+	for index, row in data.iterrows():
+		try:
+			row['StartTime'] = pd.to_datetime(unicode(row['StartTime']))
+		except ValueError as ve:
+			print 'ERROR:', ve, 'in', root, filename
+			print row
+
+		try:
+			row['EndTime'] = pd.to_datetime(unicode(row['EndTime']))
+		except ValueError as ve:
+			print 'ERROR:', ve, 'in', root, filename
+			print row
+
+
+def check_time_interval(data, root, filename):
+	# iterate over start and end times
+	for index, row in data.iterrows():
+		try:
+			row['StartTime'] = pd.to_datetime(unicode(row['StartTime']))
+		except ValueError:
+			continue
+		try:
+			row['EndTime'] = pd.to_datetime(unicode(row['EndTime']))
+		except ValueError:
+			continue
+
+		if row['StartTime'] > row['EndTime']:
+			print 'ERROR: start time after end time in', root, filename
+			print row
+
+			
+
+
+def main(rootdir):
+	# walk directory to find parse annotation csv files
+    for root, dirs, files in os.walk(rootdir):
+        for filename in files:
+            if filename.endswith('.csv'):
+
+                f = os.path.join(root,filename)
+                csv_file = open(f, 'r')
+                data = pd.read_csv(csv_file, parse_dates=True)
+                check_valid_time(data, root, filename)
+                check_time_interval(data, root, filename)
+
+
+
+
+if __name__ == '__main__':
+	rootdir = '/Users/knuth/Documents/ooi/repos/github/annotations/annotations/RS03AXPS'
+	main(rootdir)

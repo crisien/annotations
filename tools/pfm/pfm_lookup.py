909e3962f47dccbe57416cf239f62ebf2a742540
Friedrich Knuth <knuth@marine.rutgers.edu>
2017-03-24 20:25:08 -0400

    added buoy exception

--- tools/pfm/pfm_lookup.py
+++ tools/pfm/pfm_lookup.py
@@ -1,10 +1,20 @@
 import pandas as pd
-import yaml
+import re
 
-
+# define your inputs
 f = '/Users/knuth/Documents/ooi/repos/github/annotations/tools/pfm/all_params.csv'
+
 ref_des = 'RS03AXPS-PC03A-4A-CTDPFA303'
+stream = 'ctdpf_optode_sample'
+method = 'streamed' # 'recovered_host' 'telemetered' 'recovered_inst' 'recovered_cspp' 'streamed' 'recovered_wfp'
 pd_number = 'PD194'
+
+# ref_des = 'CE02SHSM-RID27-03-CTDBPC000'
+# stream = 'ctdbp_cdef_dcl_instrument'
+# method = 'telemetered' # 'recovered_host' 'telemetered' 'recovered_inst' 'recovered_cspp' 'streamed' 'recovered_wfp'
+# pd_number = 'PD923'
+
+
 
 
 
@@ -25,7 +35,17 @@ def node_check(data):
 	# subset data to node level
 	instruments_on_node = []
 	for index, row in data.iterrows():
-		if ref_des[9:14] in row['reference_designator'][9:14]:
+		if ref_des[9:14] in row['reference_designator'][9:14] and row['method'] == method:
+			instruments_on_node.append(row)
+	instruments_on_node = pd.DataFrame(instruments_on_node)
+	return instruments_on_node
+
+
+
+def buoy_check(data):
+	instruments_on_node = []
+	for index, row in data.iterrows():
+		if ref_des[:8] in row['reference_designator'][:8] and row['method'] == method:
 			instruments_on_node.append(row)
 	instruments_on_node = pd.DataFrame(instruments_on_node)
 	return instruments_on_node
@@ -35,19 +55,31 @@ def node_check(data):
 csv_file = open(f, 'r')
 data = pd.read_csv(csv_file, parse_dates=True)
 
-
 # reduce data frame to relevant columns
 columns = ['reference_designator','start_depth','end_depth','method','stream_name', 'parameter_id',
 							'name.1','parameter_function_map', 'data_product_identifier', 'data_level']
 data = data[columns]
+
+# stage list of expected PD numbers to br printed from at end of script
 affected_PDs = []
 
-possible_instruments = node_check(data)
+# create regular expression to enter buoy exception and search across nodes
+buoy_nodes = ['RID', 'SBD']
+reg_ex = re.compile('|'.join(buoy_nodes))
+
+# further reduce data frame to only those instruments that might be affected
+if reg_ex.search(ref_des):
+	possible_instruments = buoy_check(data)
+else:
+	possible_instruments = node_check(data)
+
+# begin recursive search on reduced data frame
 pfm_check(possible_instruments, pd_number)
 affected_PDs = set(affected_PDs)
 
-
-print '\nThe following PD numbers are affected and need annotation:\n'
+# print output
+print '\nThe following PD numbers use ' + pd_number + ' from ' + ref_des + '-' + stream + \
+		' and may need annotation:\n'
 for i in affected_PDs:
 	print i
 

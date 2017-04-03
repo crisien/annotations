bf4d5d0c09f416ebd439830be26e90aac07afb84
Friedrich Knuth <knuth@marine.rutgers.edu>
2017-04-03 16:07:01 -0400

    updates to redmine check tool

--- tools/check_redmine.py
+++ tools/check_redmine.py
@@ -5,30 +5,33 @@ import numpy as np
 
 
 def find_redmine_tickets(data, root, filename):
-	# iterate over start and end times
-	for index, row in data.iterrows():
-		try:
-			return int(row['Redmine#'])
-		except:
-			continue
+    # iterate over start and end times
+    file_redmine_list = []
+    for index, row in data.iterrows():
+        try:
+            # print int(row['Redmine#'])
+            x = int(row['Redmine#'])
+            file_redmine_list.append(x)
+        except:
+            continue
+    return file_redmine_list
 
 
 def main(rootdir):
-	# walk directory to find parse annotation csv files
-     redmines = []
-     for root, dirs, files in os.walk(rootdir):
+    # walk directory to find parse annotation csv files
+    redmine_list = []
+    for root, dirs, files in os.walk(rootdir):
         for filename in files:
             if filename.endswith('.csv'):
                 f = os.path.join(root,filename)
                 csv_file = open(f, 'r')
                 data = pd.read_csv(csv_file, na_values=['nan'], keep_default_na=False)
-                return_f = find_redmine_tickets(data, root, filename)
-                redmines.append(return_f)
-     for i in set(redmines):
-     	if i != None:
-     		print i
+                x = find_redmine_tickets(data, root, filename)
+                redmine_list.extend(x)
+    print set(redmine_list)
+
 
 
 if __name__ == '__main__':
-	rootdir = '/Users/knuth/Documents/ooi/repos/github/annotations/annotations'
-	main(rootdir)
+    rootdir = '/Users/knuth/Documents/ooi/repos/github/annotations/annotations'
+    main(rootdir)

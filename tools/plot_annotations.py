fc2977025ae7f67b56b708d0d11c6e1155ece44c
Friedrich Knuth <knuth@marine.rutgers.edu>
2017-04-26 17:43:15 -0400

    CE02SHBP-LJ01D-06-CTDBPN106 complete
    CE04OSPD-DP01B-01-CTDPFL105 in progress

--- tools/plot_annotations.py
+++ tools/plot_annotations.py
@@ -14,10 +14,10 @@ import numpy as np
 
 # specify path to annotation csvs, reference designator and theoretical end date for ongoing deployment, specified as 'None' in asset management
 # (you can use the date on which you downloaded the data, for example.)
-assets = '/Users/knuth/Documents/ooi/repos/github/annotations/annotations/RS01SBPS/RS01SBPS.csv'
-stream = '/Users/knuth/Documents/ooi/repos/github/annotations/annotations/RS01SBPS/RS01SBPS-SF01A-2A-CTDPFA102/streamed-ctdpf_sbe43_sample.csv'
-parameters = '/Users/knuth/Documents/ooi/repos/github/annotations/annotations/RS01SBPS/RS01SBPS-SF01A-2A-CTDPFA102/streamed-ctdpf_sbe43_sample-parameters.csv'
-reference_designator = 'RS01SBPS-SF01A-2A-CTDPFA102'
+assets = '/Users/knuth/Documents/ooi/repos/github/annotations/annotations/CE02SHBP/CE02SHBP.csv'
+stream = '/Users/knuth/Documents/ooi/repos/github/annotations/annotations/CE02SHBP/CE02SHBP-LJ01D-06-CTDBPN106/streamed-ctdbp_no_sample.csv'
+parameters = '/Users/knuth/Documents/ooi/repos/github/annotations/annotations/CE02SHBP/CE02SHBP-LJ01D-06-CTDBPN106/streamed-ctdbp_no_sample_parameters.csv'
+reference_designator = 'CE02SHBP-LJ01D-06-CTDBPN106'
 # ongoing_dep_end = '2017-03-21T00:00:00'
 
 ## use this to manually specify deployment start and end times. comment block using ongoing_dep_end out accordingly.

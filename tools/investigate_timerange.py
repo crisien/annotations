a50b5ea0f8661713891e1fade0934842ddc6afea
leilabbb <leila.ocean@gmail.com>
2017-05-16 16:35:26 -0400

    updated tool

--- tools/investigate_timerange.py
+++ tools/investigate_timerange.py
@@ -12,15 +12,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-nc_file = '/Users/leila/Downloads/deployment0003_CE04OSPS-PC01B-4A-CTDPFA109-streamed-ctdpf_optode_sample_20161228T000000.163503-20170321T235959.713428.nc'
+nc_file = '/Users/leila/Downloads/deployment0004_CP02PMUO-WFP01-03-CTDPFK000-recovered_wfp-ctdpf_ckl_wfp_instrument_recovered_20141005T220003-20150212T211238.987984.nc'
 
-deployment_num = 'deployment[3]'
+deployment_num = 'deployment[4]'
 time_variable = 'time'
-param_variable = 'seawater_temperature'
-ylim_min = -2
-ylim_max = 10
-date_0 = datetime.datetime(2017, 2, 7, 13, 0, 0)
-date_n = datetime.datetime(2017, 2, 7, 15, 0, 0)
+param_variable = 'ctdpf_ckl_seawater_pressure'
+
+date_0 = datetime.datetime(2014, 11, 13, 0, 0, 0)
+date_n = datetime.datetime(2014, 11, 27, 0, 0, 0)
 
 
 f = nc.Dataset(nc_file)
@@ -38,6 +37,9 @@ print time_xx
 
 parameter_yy = y_parameter[ind]
 print parameter_yy
+
+ylim_min = min(parameter_yy)
+ylim_max = max(parameter_yy)
 
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%dT%H:%M:%SZ'))
 plt.gca().xaxis.set_major_locator(mdates.DayLocator())
